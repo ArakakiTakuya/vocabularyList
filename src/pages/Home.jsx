ce3c47e64a6db9ddb077888ac9492ade64a9df83
ArakakiTakuya <225arataku@gmail.com>
2020-08-05 23:07:17 +0900

    wrote Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import "../styles/home.css";
+
 const Home = () => {
-  return <></>;
+  return (
+    <div>
+      <div className="toppage">
+        <img
+          className="topImage"
+          src="../../images/top-image.png"
+          alt="トップ画像"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
