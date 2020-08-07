7ad1beb835dc8df53a1769992fe5d60a9ea7a605
ArakakiTakuya <225arataku@gmail.com>
2020-08-07 23:25:03 +0900

    set create-list component

--- src/pages/CreateList.jsx
+++ src/pages/CreateList.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import TextInput from "../components/UIKit/TextInput";
+import Icon from "@material-ui/core/Icon";
+import "../styles/create-list.css";
+
 const CreateList = () => {
-  return <></>;
+  return (
+    <div className="create-list">
+      <h2>新しい単語帳を作る</h2>
+      <TextInput
+        fullWidth={true}
+        label={"単語帳の名前: "}
+        multiline={false}
+        required={true}
+        rows={1}
+      />
+      <TextInput fullWidth={true} label={"説明: "} multiline={false} rows={1} />
+      <Card className="card">
+        <CardContent>
+          <TextInput label={"単語: "} multiline={false} rows={1} />
+          <TextInput label={"意味: "} multiline={false} rows={1} />
+        </CardContent>
+      </Card>
+      <Card className="card">
+        <CardContent>
+          <TextInput label={"単語: "} multiline={false} rows={1} />
+          <TextInput label={"意味: "} multiline={false} rows={1} />
+        </CardContent>
+      </Card>
+      <Card className="card">
+        <CardContent>
+          <TextInput label={"単語: "} multiline={false} rows={1} />
+          <TextInput label={"意味: "} multiline={false} rows={1} />
+        </CardContent>
+      </Card>
+      <Icon className="addIcon" color="error" style={{ fontSize: 60 }}>
+        add_circle
+      </Icon>
+    </div>
+  );
 };
 
 export default CreateList;
