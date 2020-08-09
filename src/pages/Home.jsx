36935bc40f918b1933ce64dab6df9282541021e0
ArakakiTakuya <225arataku@gmail.com>
2020-08-09 19:54:46 +0900

    fetch data from firestore in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { signOut } from "../reducks/users/operations";
+import { db } from "../firebase/index";
 
 const Home = () => {
   const dispatch = useDispatch();
   const selector = useSelector((state) => state);
   const uid = selector.users.uid;
   const username = selector.users.username;
+
+  const [lists, setLists] = useState([]);
+  console.log(lists);
+
+  useEffect(() => {
+    db.collection("lists")
+      .where("creatorId", "==", uid)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          console.log("No matching documents.");
+          return;
+        }
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          setLists((prevState) => [...prevState, data]);
+        });
+      });
+  }, []);
+
   return (
     <div>
       <p>{uid}</p>
