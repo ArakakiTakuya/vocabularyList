39c06a9f99bac60c3f5febf1621651ca15e37e10
ArakakiTakuya <225arataku@gmail.com>
2020-08-10 20:16:09 +0900

    added dialog for deleting a word

--- src/components/VocabularyList.jsx
+++ src/components/VocabularyList.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Paper } from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import Button from "@material-ui/core/Button";
 
 import "../styles/vocabulary-list.css";
 
 const VocabularyList = ({ words }) => {
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpenDialog(true);
+  };
+
+  const handleClose = () => {
+    setOpenDialog(false);
+  };
+
   return (
     <div>
       <h2 className="title">収録単語一覧({words.length})</h2>
@@ -15,18 +30,33 @@ const VocabularyList = ({ words }) => {
             <h4>{word.word}</h4>
             <h5> {word.meaning}</h5>
             <div className="delete_btn">
-              <IconButton
-                aria-label="delete"
-                onClick={() => {
-                  console.log("hi");
-                }}
-              >
+              <IconButton aria-label="delete" onClick={handleClickOpen}>
                 <DeleteIcon />
               </IconButton>
             </div>
           </Paper>
         ))}
       </div>
+      <>
+        <Dialog open={openDialog} onClose={handleClose}>
+          <DialogContent>
+            <DialogContentText>この単語を消去しますか？</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              No
+            </Button>
+            <Button
+              onClick={() => {
+                console.log("wow");
+              }}
+              color="primary"
+            >
+              Yes
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
     </div>
   );
 };
